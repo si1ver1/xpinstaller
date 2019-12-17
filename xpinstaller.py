23d7a59dc000bf47ed4cc1383ecfa2e1ece94ecd
Adam <silvertitaniumx@gmail.com>
2019-12-16 23:07:52 -0800

    added multithreaded downloads

--- xpinstaller.py
+++ xpinstaller.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 import os
-import zipfile
 import requests
 from datetime import datetime
+from time import time as timer
+from multiprocessing.pool import ThreadPool
+from shutil import rmtree
 
 try:
     import httplib
@@ -20,20 +22,39 @@ def internet_on():
         conn.close()
         return False
 
-def log():
-    with open('installer.log', 'a') as f:
-        print(dt_string, 'Starting XchangePoint Installer', file = f)
-        print(dt_string, 'Checking network connection:', internet_on(), file=f)
+def url_response(entry):
+    path, uri = entry
+    if not os.path.exists(path):
+        r = requests.get(uri, stream=True)
+        if r.status_code == 200:
+            with open(path, 'wb') as f:
+                for chunk in r:
+                    f.write(chunk)
+    return path
 
-def download():
-    with open('installer.log', 'a') as f:
-        print(dt_string, 'Downloading XchangePoint', file = f)
-    url = 'http://files.taskretail.com.au/Aramark/1905/XP1905_20191025.zip'
-    r = requests.get(url)
-    with open('./', 'wb') as l:
-        l.write(r.content)
+
 
 now = datetime.now()
 dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
-log()
-download()
+urls = [
+("./temp/1mb.txt", "https://rantcell.com/1mb.txt"),
+("./temp/5mb.txt", "https://rantcell.com/5mb.txt"),
+("./temp/10mb.txt", "https://rantcell.com/10mb.txt")
+]
+
+print(dt_string, 'Starting XchangePoint Installer')
+print(dt_string, 'Checking network connection:', internet_on())
+print(dt_string, 'Downloading files')
+
+# Create temp folder and download files into it
+if not os.path.exists("./temp"):
+    os.mkdir("./temp")
+
+start = timer()
+results = ThreadPool(4).imap_unordered(url_response, urls)
+for path in results:
+    print(path)
+print("Elapsed Time: %s" % (timer() - start,))
+
+# Remove temp folder and files
+# rmtree("./temp")
